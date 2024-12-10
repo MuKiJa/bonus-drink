034a822581422cd3996dccac61b95f6f22cb98a1
MuKiJa <kim.janka.mueller@alum.uol.de>
2024-12-10 11:42:32 +0100

    Mueller_answer_bonus_drink.rb

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,19 @@
 class BonusDrink
-  def self.total_count_for(amount)
-    0
-  end
+ 
+# ask for the number of bottles
+puts "Please enter the number of purchased bottles."
+number = gets.chomp.to_i
+puts "You purchased #{number} bottles."
+
+#variables
+#y_divider = number.to_f / 3
+y_divider = number/3
+# transform into an integer
+y_divider.to_i
+puts "You get #{y_divider} bottle(s) extra!"
+
+# sum up the number of purchase bottles + extra bottles
+sum = number+y_divider
+puts "The total number of bottles is #{sum}."
+  
 end
